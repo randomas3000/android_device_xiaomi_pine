30f85c6221742ebe229bf0e612a9320310350ee5
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-03 22:08:29 +0800

    CherishOS: pine: Create cherish makefile

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     lineage_pine-user \
     lineage_pine-userdebug \
     lineage_pine-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/cherish_pine.mk
+
+COMMON_LUNCH_CHOICES += \
+    cherish_pine-user \
+    cherish_pine-userdebug \
+    cherish_pine-eng
