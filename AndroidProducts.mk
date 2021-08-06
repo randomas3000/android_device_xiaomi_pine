5899df4ef0faae3e1747718025cf305ec50e0bcd
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-06 13:07:07 +0800

    RevengeOS: pine: Create revengeos makefile

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     lineage_pine-user \
     lineage_pine-userdebug \
     lineage_pine-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/revengeos_pine.mk
+
+COMMON_LUNCH_CHOICES += \
+    revengeos_pine-user \
+    revengeos_pine-userdebug \
+    revengeos_pine-eng
